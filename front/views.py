ceb4d7851522e13ff1f88268efd55a473fc7a2b3
Mutaf <mutaf.dev@gmail.com>
2025-05-07 14:08:25 +0300

    fix templates

--- front/views.py
+++ front/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
 
+from config.settings import CURRENT_HOST
+
+
 def comments(request):
-    return render(request, 'comments.html')
+    return render(request, 'comments.html', context={'current_host': CURRENT_HOST})
